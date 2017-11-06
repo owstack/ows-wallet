0d3302716962d22c57798262b044702ec9f6c296
Andy Phillipson <andy@bitpay.com>
2017-11-06 17:20:34 -0500

    Brand resources update

--- resources/generate.sh
+++ resources/generate.sh
@@ -1,37 +1,43 @@
 # This script requires Sketch on macOS – see readme.md for details
 
-# export all slices marked for export to the proper directory
-echo "Exporting all assets from src.sketch..."
-# sketchtool is installed by install.sh
-sketchtool export layers src.sketch
-
 function postprocess {
-  # $1 = distribution name
-  echo "Beginning postprocessing for $1..."
+  echo "Beginning postprocessing for app..."
 
   echo "Postprocessing assets for macOS..."
-  iconset $1 app
-  iconset $1 volume-icon
+  iconset app
+  iconset volume-icon
 
   echo "Creating Retina-ready DMG background..."
-  tiffutil -cathidpicheck $1/mac/dmg-background.png $1/mac/dmg-background@2x.png -out $1/mac/dmg-background.tiff
+  tiffutil -cathidpicheck app/mac/dmg-background.png app/mac/dmg-background@2x.png -out app/mac/dmg-background.tiff
   echo "Removing raw background pngs..."
-  rm $1/mac/dmg-background.png $1/mac/dmg-background@2x.png
+  rm app/mac/dmg-background.png app/mac/dmg-background@2x.png
 
   echo "Postprocessing assets for Windows..."
 
   echo "Combining windows/ico pngs into a single ICO file..."
   # convert ships with imagemagick
-  convert $1/windows/ico/ico_16x16.png $1/windows/ico/ico_24x24.png $1/windows/ico/ico_32x32.png $1/windows/ico/ico_48x48.png $1/windows/ico/ico_64x64.png $1/windows/ico/ico_128x128.png $1/windows/ico/ico_256x256.png $1/windows/icon.ico
+  convert app/windows/ico/ico_16x16.png app/windows/ico/ico_24x24.png app/windows/ico/ico_32x32.png app/windows/ico/ico_48x48.png app/windows/ico/ico_64x64.png app/windows/ico/ico_128x128.png app/windows/ico/ico_256x256.png app/windows/icon.ico
   echo "Removing raw windows/ico pngs..."
-  rm -r $1/windows/ico/* && rmdir $1/windows/ico
+  rm -r app/windows/ico/* && rmdir app/windows/ico
 }
 
 function iconset {
-  echo "Converting $1 $2 iconset to icns..."
-  iconutil --convert icns $1/mac/$2.iconset --output $1/mac/$2.icns
-  echo "Removing $1 $2 iconset..."
-  rm -r $1/mac/$2.iconset
+  echo "Converting $1 iconset to icns..."
+  iconutil --convert icns app/mac/$1.iconset --output app/mac/$1.icns
+  echo "Removing $1 iconset..."
+  rm -r app/mac/$1.iconset
 }
 
-postprocess ows
+# export all slices marked for export to the proper directory
+echo "Exporting all assets from src.sketch..."
+
+# Remove existing resources
+rm -fr ./ows-wallet
+
+# sketchtool is installed by install.sh
+sketchtool export layers src.sketch
+
+postprocess
+
+# Rename resources dir for app-config
+mv ./app ./ows-wallet
